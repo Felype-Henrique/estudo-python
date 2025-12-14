c984e5d9713d006db6a6b90cc808c6b3ca402eae
Felype Henrique <felypehenrique25@gmail.com>
2025-12-14 10:52:55 -0300

    aula 02 - keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,36 @@
+"""" Aula 02 - Keywords e indentificadores """
+
+# Palavras reservadas
+# True, False, None, Not, import , lambda
+
+# Identificadores
+# nome de variáveis, funcções, classes
+# cadse sensitive
+# devem iniciar com letra ou _
+# não podem conter espaços em branco
+# não pode usar caracteres especiais
+
+nome = 'Maria'
+idade = 30
+nome = 'João'
+nome_completo = 'Maria da Silva'
+
+# Boas práticas
+
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+PI = 3.14
+
+idade = 18
+MAIORIDADE = 18
+
+
+if idade >= MAIORIDADE:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
