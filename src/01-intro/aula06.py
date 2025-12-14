1cbef03b6d428da2d495e196e15c28b35790b5d1
Felype Henrique <felypehenrique25@gmail.com>
2025-12-14 13:26:29 -0300

    aula 06 - conversao de tipos

--- src/01-intro/aula06.py
+++ src/01-intro/aula06.py
@@ -11,3 +11,13 @@ print(total, type(total))
 
 soma = 13.4 + float('3.5')
 print(soma, type(soma))
+
+idade = int('32')
+print(idade, type(idade))
+
+nome = 'Maria'
+altura = 1.70
+
+# mensagem = nome + 'tem a altura de ' + str(altura)
+mensagem = f'{nome} tem a altura de {altura}'
+print(mensagem)
