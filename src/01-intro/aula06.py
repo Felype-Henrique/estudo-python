87d67d76caa382fe56cb2f604a475eddbf9e02d5
Felype Henrique <felypehenrique25@gmail.com>
2025-12-14 13:25:19 -0300

    conversao de tipos

---
+++ src/01-intro/aula06.py
@@ -0,0 +1,13 @@
+""" Conversão de tipos """
+
+# Conversão implicita
+
+leitura = 5.53
+peso = 3
+total = leitura * peso
+print(total, type(total))
+
+# Conversão explicita (type casting)
+
+soma = 13.4 + float('3.5')
+print(soma, type(soma))
